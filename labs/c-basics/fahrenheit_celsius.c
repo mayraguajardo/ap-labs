1caaa538d9a7640536bbf0d983d30b7983a5aeb3
Mayra Guajardo <A01634475@itesm.mx>
2020-08-25 03:44:37 +0000

    Solve c-basics lab fahrenheit to celsius
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
+#include <stdlib.h>
+#include <string.h>
 
 /* print Fahrenheit-Celsius table */
-
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+   int fahr;
+    if (argc-1 == 1){
+        fahr = atoi(argv[1]);
+        printf("Fahrenheit: %3d, Celsius: %6.1f\n",fahr,(5.0/9.0)*(fahr-32));
+    }
+    else if (argc-1 == 3){
+        int LOWER = atoi(argv[1]);
+        int UPPER = atoi(argv[2]);
+        int STEP = atoi(argv[3]);
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    } 
+    else{
+        printf("No valid arguments\n");
+    }
 
     return 0;
 }
