d3a6276e6f2cd59b3fabb6c5646f27c3f765f38d
Mayra Guajardo <A01634475@itesm.mx>
2020-09-01 18:51:15 +0000

    Solve word-reverser lab
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void reverse(void)
+{
+    char c;
+    if((c = getchar()) != '\n'){ reverse(); }
+    putchar(c);
+    return;
+}
 int main(){
     // Place your magic here
+
+    printf("Enter a string:\n");
+    reverse();
+    putchar('\n'); 
     return 0;
+
 }
