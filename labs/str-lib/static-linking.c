e9bf44dd03834ce5ed94ef04859c07c350ed2654
Mayra Guajardo <A01634475@itesm.mx>
2020-09-09 03:12:45 +0000

    Solve str-lib lab
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int mystrlen(char *string);
+char* mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv) {
+    if (argc == 4 && strcmp(argv[1], "-add") == 0){
+        char* origin = argv[2];
+        char* addition = argv[3];
+        printf("Initial length: %i \n", mystrlen(origin));
+        char* new = mystradd(origin, addition);
+        printf("New String: %s \n", new);
+        printf("New length: %i \n", mystrlen(new));
+    } else if (argc == 4 && strcmp(argv[1], "-find") == 0){
+        char* origin = argv[2];
+        char* substring = argv[3];
+        int position = mystrfind(origin, substring);
+        if (position != -1){
+            printf("[%s] string was found at [%i] position. \n", substring, position);
+        } else {
+            printf("[%s] string was not found in the string [%s]. \n", substring, origin);
+        }
+        
+    } else {
+        printf("Supported arguments are -add or -find only, along with two strings.\n");
+        return 0;
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,55 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int i;
+    for (i = 0; str[i] != '\0'; i++);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *new = malloc(mystrlen(origin) + mystrlen(addition) + 1);
+    int i;
+    for (i = 0; origin[i] != '\0'; i++){
+        new[i] = origin[i];
+    }
+    for (int x = 0; addition[x] != '\0'; ++x, ++i){
+        new[i] = addition[x];
+    }
+    return new;
 }
 
 int mystrfind(char *origin, char *substr){
+    int i = 0, j = 0, firstOcurrence;
+    while (origin[i] != '\0'){
+        //looping through origin word to find a first coincidence
+        while(origin[i] != substr[0] && origin[i] != '\0'){
+            i++;
+        }
+
+        //not a single coincidence found
+        if (origin[i] == '\0'){
+            return -1;
+        }
+        //mark the occurrence place
+        firstOcurrence = i;
+        //keep comparing chars until we reach the end of any word
+        while(origin[i] == substr[j] && origin[i] != '\0' && substr[j] != '\0'){
+            i++;
+            j++;
+        }
+        //if we reached the end of the substring first or at the same time, then it is a substring of origin
+        if (substr[j] == '\0'){
+            return firstOcurrence;
+        }
+        //otherwise, if we reached the end of origin without finding the end of substring, then it's nto one
+        if (origin[i] == '\0'){
+            return -1;
+        }
+        //change our i position to continue where we left of, and j to 0 to compare the whole substring again
+        i = firstOcurrence + 1;
+        j = 0;
+
+
+    }
     return 0;
 }
