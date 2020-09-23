a0a7c93e91f80acc194cad01fcc74e745d8696ba
Mayra Guajardo <A01634475@itesm.mx>
2020-09-23 02:46:38 +0000

    Solve go-slices-maps lab
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	mapa := make(map[string]int)
+	for i := range words{
+		mapa[words[i]]++
+	}
+	return mapa
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,15 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	pic := make([][] uint8,dy)
+	for x := 0; x < dy; x++{
+		pic[x] = make([]uint8, dx)
+		for y := 0; y < dx; y++{
+			pic[x][y] = (uint8)(x^y)
+		}
+	}
+
+     return pic
 }
 
 func main() {
