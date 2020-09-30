8595d712c8b000a05bac88f5e66d2eb17b14262e
Mayra Guajardo <A01634475@itesm.mx>
2020-09-30 03:26:10 +0000

    Solve advanced-logger lab
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -4,32 +4,38 @@
 
 int main(int argc, char **argv){
 
-    printf("%d\n", atoi(argv[1]));
-    switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
-    }
+	if(argc > 1) {
+		printf("%d\n", atoi(argv[1]));
+		switch(atoi(argv[1])) {
+		case 1:
+		// default logging
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 2:
+		// stdout logging
+		initLogger("stdout");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 3:
+		// syslog logging
+		initLogger("syslog");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		default:
+		errorf("Invalid test case");
+		break;
+		}
+	} else{
+		errorf("Not enough arguments.");
+	}
+
+    
 
     return 0;
 }
