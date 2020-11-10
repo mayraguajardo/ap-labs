1e4cc9381e811546dfc7f174df347cc4d6f1de31
Mayra Guajardo <A01634475@itesm.mx>
2020-11-10 07:44:34 +0000

    Solve goroutines vs os threads lab
    Signed-off-by: Mayra Guajardo <A01634475@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"os"
 )
-
 func main() {
+	channel := make(chan struct{})
+	msg := make(chan int)
 	var commsPerSecond int
+	startTime := time.Now()
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+	go func(){
+		msg <- 1
+		for{
+			commsPerSecond++
+			msg<- <-msg
+		}
+		channel <- struct{}{}
+	}()
+	go func(){
+		for{
+			commsPerSecond++
+			msg<- <-msg
+		}
+		channel <- struct{}{}
+	}()
+	for{
+		select{
+		case <- time.After(1 * time.Second):
+			s := float64(commsPerSecond)/time.Since(startTime).Seconds()
+			fmt.Println("Communications Per Second :", s)
+		}
+		if commsPerSecond > 50000000{
+			fmt.Println("Communication is over")
+			os.Exit(0)
+		}
+	}
+
+	<- channel
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var channel = make(chan struct{})
+var transitTime = time.Now()
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+func pipeline(first chan struct{}, second chan struct{}, cuStages int, maxStages int){
+	if cuStages <= maxStages{
+		fmt.Println("Maximum number of pipeline stages   : ", maxStages)
+		fmt.Println("Time to transit trough the pipeline : ", time.Since(transitTime))
+		transitTime = time.Now()
+		go pipeline(second,first,cuStages+1,maxStages)
+		second<-<- first
+	}else{
+		fmt.Println("Maximum number of stages, need to wait")
+		channel <- struct{}{}
+	}
+}
+func main() {
+	var firstPipeline chan(struct{})
+	var secondPipeline chan(struct{})
+	go pipeline(firstPipeline, secondPipeline, 0, 100)
+	<-channel
 }
